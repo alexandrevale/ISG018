3ee304fd12d1e9e43a167f16e4923e5fde96e97c
Alexandre do Vale <vale.apv@gmail.com>
2017-08-14 23:44:16 +0000

    Organização do workspace

--- cool-tricks/cool-tricks.py
+++
@@ -1,7 +0,0 @@
-reverso = "Phyton is cool"
-print "Invert: ",reverso[::-1]
-
-separado=["Unindo", "varios", "elementos", "em", "uma", "unica", "variavel"]
-print separado
-juntinho = " ".join(separado)
-print juntinho
